0f385e442f880e99e87aebff43cac313566bdc82
Joel Wilsson <joel.wilsson@gmail.com>
2017-01-22 13:06:34 +0100

    Use primitive types with gen-class

--- src/bean_benchmarks/gen_class_bean.clj
+++ src/bean_benchmarks/gen_class_bean.clj
@@ -8,21 +8,21 @@
  :state state
  :prefix "edit-"
  :constructors {[] []
-                [Long
+                [long
                  String
                  String
                  String
                  String
-                 Long
+                 long
                  String
-                 Boolean
-                 Boolean
-                 Boolean
-                 Boolean
-                 Boolean
-                 Boolean] []}
- :methods [[getTimestamp [] Long]
-           [setTimestamp [Long] void]
+                 boolean
+                 boolean
+                 boolean
+                 boolean
+                 boolean
+                 boolean] []}
+ :methods [[getTimestamp [] long]
+           [setTimestamp [long] void]
            [getChannel [] String]
            [setChannel [String] void]
            [getTitle [] String]
@@ -31,22 +31,22 @@
            [setDiffUrl [String] void]
            [getUser [] String]
            [setUser [String] void]
-           [getByteDiff [] Long]
-           [setByteDiff [Long] void]
+           [getByteDiff [] long]
+           [setByteDiff [long] void]
            [getSummary [] String]
            [setSummary [String] void]
-           [getIsMinor [] Boolean]
-           [setIsMinor [Boolean] void]
-           [getIsNew [] Boolean]
-           [setIsNew [Boolean] void]
-           [getIsUnpatrolled [] Boolean]
-           [setIsUnpatrolled [Boolean] void]
-           [getIsBotEdit [] Boolean]
-           [setIsBotEdit [Boolean] void]
-           [getIsSpecial [] Boolean]
-           [setIsSpecial [Boolean] void]
-           [getIsTalk [] Boolean]
-           [setIsTalk [Boolean] void]])
+           [getIsMinor [] boolean]
+           [setIsMinor [boolean] void]
+           [getIsNew [] boolean]
+           [setIsNew [boolean] void]
+           [getIsUnpatrolled [] boolean]
+           [setIsUnpatrolled [boolean] void]
+           [getIsBotEdit [] boolean]
+           [setIsBotEdit [boolean] void]
+           [getIsSpecial [] boolean]
+           [setIsSpecial [boolean] void]
+           [getIsTalk [] boolean]
+           [setIsTalk [boolean] void]])
 
 (defn edit-init
   ([] (edit-init 0 nil nil nil nil 0 nil false false false false false false))
@@ -63,72 +63,63 @@
     isBotEdit
     isSpecial
     isTalk]
-   [[] (object-array [timestamp
-                      channel
-                      title
-                      diffUrl
-                      user
-                      byteDiff
-                      summary
-                      isMinor
-                      isNew
-                      isUnpatrolled
-                      isBotEdit
-                      isSpecial
-                      isTalk])]))
+   [[] (object-array [(long-array [timestamp byteDiff])
+                      (object-array [channel title diffUrl user summary])
+                      (boolean-array [isMinor isNew isUnpatrolled
+                                      isBotEdit isSpecial isTalk])])]))
 
 (defn edit-getTimestamp [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 0))
+  (aget ^longs (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 0) 0))
 (defn edit-setTimestamp [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 0 ^Long v))
+  (aset ^longs (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 0) 0 ^long v))
 (defn edit-getChannel [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1))
+  (aget ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 0))
 (defn edit-setChannel [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 1 ^String v))
+  (aset ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 0 ^String v))
 (defn edit-getTitle [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2))
+  (aget ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 1))
 (defn edit-setTitle [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 2 ^String v))
+  (aset ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 1 ^String v))
 (defn edit-getDiffUrl [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 3))
+  (aget ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 2))
 (defn edit-setDiffUrl [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 3 ^String v))
+  (aset ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 2 ^String v))
 (defn edit-getUser [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 4))
+  (aget ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 3))
 (defn edit-setUser [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 4 ^String v))
+  (aset ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 3 ^String v))
 (defn edit-getByteDiff [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 5))
+  (aget ^longs (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 0) 1))
 (defn edit-setByteDiff [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 5 ^Long v))
+  (aset ^longs (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 0) 1 ^long v))
 (defn edit-getSummary [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 6))
+  (aget ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 4))
 (defn edit-setSummary [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 6 ^String v))
+  (aset ^objects (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 1) 4 ^String v))
 (defn edit-getIsMinor [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 7))
+  (aget ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 0))
 (defn edit-setIsMinor [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 7 ^Boolean v))
+  (aset ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 0 ^boolean v))
 (defn edit-getIsNew [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 8))
+  (aget ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 1))
 (defn edit-setIsNew [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 8 ^Boolean v))
+  (aset ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 1 ^boolean v))
 (defn edit-getIsUnpatrolled [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 9))
+  (aget ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 2))
 (defn edit-setIsUnpatrolled [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 9 ^Boolean v))
+  (aset ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 2 ^boolean v))
 (defn edit-getIsBotEdit [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 10))
+  (aget ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 3))
 (defn edit-setIsBotEdit [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 10 ^Boolean v))
+  (aset ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 3 ^boolean v))
 (defn edit-getIsSpecial [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 11))
+  (aget ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 4))
 (defn edit-setIsSpecial [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 11 ^Boolean v))
+  (aset ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 4 ^boolean v))
 (defn edit-getIsTalk [^bean_benchmarks.EditGenClass this]
-  (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 12))
+  (aget ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 5))
 (defn edit-setIsTalk [^bean_benchmarks.EditGenClass this v]
-  (aset ^objects (.state ^bean_benchmarks.EditGenClass this) 12 ^Boolean v))
+  (aset ^booleans (aget ^objects (.state ^bean_benchmarks.EditGenClass this) 2) 5 ^boolean v))
 
 (def persist-and-load (beans/make-persist-and-load bean_benchmarks.EditGenClass.
                                                    bean_benchmarks.EditGenClass))
--- src/bean_benchmarks/macro_bean.clj
+++ src/bean_benchmarks/macro_bean.clj
@@ -7,6 +7,25 @@
       last
       (str "-")))
 
+(def primitives
+  {'boolean {:default false :array-type 'booleans}
+   'byte {:default (byte 0) :array-type 'bytes}
+   'char {:default (char \u0000) :array-type 'chars}
+   'short {:default (short 0) :array-type 'shorts}
+   'int {:default (int 0) :array-type 'ints}
+   'long {:default (long 0) :array-type 'longs}
+   'float {:default (float 0) :array-type 'floats}
+   'double {:default (double 0) :array-type 'doubles}})
+
+(defn default-value [type]
+  (get-in primitives [type :default] nil))
+
+(defn array-type [type]
+  (get-in primitives [type :array-type] 'objects))
+
+(defn primitive? [type]
+  (contains? primitives type))
+
 (defn sym->upcase-1-str
   "Converts the first character of a symbol's name to uppercase, returns a string"
   [sym]
@@ -24,34 +43,73 @@
     `([~(symbol (str "get" sym-name)) [] ~type]
       [~(symbol (str "set" sym-name)) [~type] ~(symbol "void")])))
 
-(defn type-and-field->methods
-  [[type field-name] class-name field-num]
+(defn type-and-field->accessors
+  [[type field-name] class-name [type-array-index field-index]]
   (let [prefix (->prefix class-name)
         sym-name (sym->upcase-1-str field-name)
         value-sym (tagged-sym field-name type)
         this-sym (tagged-sym "this" class-name)
-        state-sym (tagged-sym "state" 'objects)]
+        state-sym (tagged-sym "state" 'objects)
+        array-sym (tagged-sym "type-array" (array-type type))]
     `((defn ~(symbol (str prefix "get" sym-name)) [~this-sym]
-        (let [~state-sym (.state ~this-sym)]
-          (aget ~state-sym ~field-num)))
-      (defn ~(symbol (str prefix "set" sym-name)) [~this-sym ~value-sym]
-        (let [~state-sym (.state ~this-sym)]
-         (aset ~state-sym ~field-num ~value-sym))))))
+        (let [~state-sym (.state ~this-sym)
+              ~array-sym (aget ~state-sym ~type-array-index)]
+          (aget ~array-sym ~field-index)))
+      (defn ~(symbol (str prefix "set" sym-name)) [~this-sym ~(symbol field-name)]
+        (let [~state-sym (.state ~this-sym)
+              ~array-sym (aget ~state-sym ~type-array-index)]
+          (aset ~array-sym ~field-index ~value-sym))))))
+
+(defn primitive-field->initializer [[type fields]]
+  (let [array-type (symbol (str "clojure.core/" (name type) "-array"))]
+    `(~array-type [~@fields])))
+
+(defn initial-state-value [types-and-fields]
+  (let [type->fields (into {} types-and-fields)
+        non-primitives (->> (filter #(not (primitive? (first %))) types-and-fields)
+                            (map second)
+                            sort)
+        primitive-types-and-fields (->> (filter #(primitive? (first %)) types-and-fields)
+                                        (map (fn [[type field-name]]
+                                               {type [field-name]})))
+        primitive-type->fields (apply merge-with concat primitive-types-and-fields)
+        type->index (->> (map vector (sort (keys primitive-type->fields)) (range))
+                         (into {'object (count primitive-type->fields)}))
+        primitive-inits (->> (map primitive-field->initializer primitive-type->fields)
+                             (sort-by first))
+        field-name->coordinate (->> (map (fn [[type field-name]]
+
+                                           (let [type-fields (or (get primitive-type->fields type)
+                                                                 non-primitives)]
+                                             {field-name [(type->index (if (primitive? type)
+                                                                         type
+                                                                         'object))
+                                                          (.indexOf ^java.util.List type-fields
+                                                                    field-name)]}))
+                                         types-and-fields)
+                                    (into {}))
+        object-inits `(object-array [~@non-primitives])]
+    [field-name->coordinate (concat primitive-inits [object-inits])]))
 
 (defmacro defbean
   "Generates a Java bean. The fields are a set of field type and name."
-  [class-name typed-fields]
-  {:pre [(vector? typed-fields)
-         (even? (count typed-fields))]}
+  [class-name types-and-fields]
+  {:pre [(vector? types-and-fields)
+         (every? vector? types-and-fields)
+         (every? (comp even? count) types-and-fields)]}
   (let [prefix (->prefix class-name)
-        types-and-fields (partition 2 typed-fields)
         field-types (map first types-and-fields)
         field-names (map second types-and-fields)
+        val (initial-state-value types-and-fields)
+        [field-name->coordinate initial-state-initializer] (initial-state-value types-and-fields)
         method-decls (mapcat type-and-field->declarations types-and-fields)
-        method-impls (mapcat (fn [type-and-field i]
-                               (type-and-field->methods type-and-field class-name i))
-                          types-and-fields (range))
-        initial-state (gensym "initial-state")]
+        method-impls (mapcat (fn [[type field-name :as type-and-field]]
+                               (type-and-field->accessors type-and-field class-name
+                                                          (field-name->coordinate field-name)))
+                             types-and-fields)
+        initial-state (gensym "initial-state")
+        init-sym (symbol (str prefix "init"))
+        null-constructor-values (map default-value field-types)]
     `(do
        (gen-class
         :name ~class-name
@@ -63,30 +121,27 @@
                        [~@field-types] []}
         :methods [~@method-decls])
 
-       (defn ~(symbol (str prefix "init"))
-         ([] [[] (object-array ~(count field-names))])
+       (defn ~init-sym
+         ([] [[] (~init-sym ~@null-constructor-values)])
          ([~@(map symbol field-names)]
-          (let [~initial-state (object-array ~(count field-names))]
-            ~@(->> (map vector field-names (range))
-                   (map (fn [[field idx]] `(aset ~initial-state ~idx ~(symbol field)))))
-            [[] ~initial-state])))
+          [[] (object-array [~@initial-state-initializer])]))
 
        ~@method-impls)))
 
 (defbean bean_benchmarks.MacroGenClass
-  [Long timestamp
-   String channel
-   String title
-   String diffUrl
-   String user
-   Long byteDiff
-   String summary
-   Boolean isMinor
-   Boolean isNew
-   Boolean isUnpatrolled
-   Boolean isBotEdit
-   Boolean isSpecial
-   Boolean isTalk])
+  [[long timestamp]
+   [String channel]
+   [String title]
+   [String diffUrl]
+   [String user]
+   [long byteDiff]
+   [String summary]
+   [boolean isMinor]
+   [boolean isNew]
+   [boolean isUnpatrolled]
+   [boolean isBotEdit]
+   [boolean isSpecial]
+   [boolean isTalk]])
 
 (def persist-and-load (beans/make-persist-and-load bean_benchmarks.MacroGenClass.
                                                    bean_benchmarks.MacroGenClass))
